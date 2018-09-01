cf0144c4de2511cead6189ce58ee071467639994
Lucas C Wilcox <lucas@swirlee.com>
2018-09-01 15:23:57 +0000

    Turn off precompiling for now
    We seem to be getting error messages from precompiling since depending
    on MPI.jl.  So we are going to turn it off for now.
    
    The long term solution may be to have an abstract MPI interface so that
    Canary doesn't have to depend directly on the MPI package.

--- src/Canary.jl
+++ src/Canary.jl
@@ -1,5 +1,12 @@
 module Canary
 
+# We seem to be getting error messages from precompiling since depending on
+# MPI.jl.  So we are going to turn it off for now.
+#
+# The long term solution may be to have an abstract MPI interface so that
+# Canary doesn't have to depend directly on the MPI package.
+__precompile__(false)
+
 export brickmesh, centroidtocode
 
 include("mesh.jl")
