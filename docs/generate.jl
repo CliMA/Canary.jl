72264de4f4c7879fbbcf7aea2ab4e89345121d2f
Lucas C Wilcox <lucas@swirlee.com>
2018-10-03 14:16:31 -0700

    Copy image asset for docs

--- docs/generate.jl
+++ docs/generate.jl
@@ -5,9 +5,12 @@ EXAMPLEDIR = joinpath(@__DIR__, "..", "examples")
 EXAMPLES = [joinpath(EXAMPLEDIR, f) for f in ("advection.jl",)]
 GENERATEDDIR = joinpath(@__DIR__, "src", "examples", "generated")
 
+
 mkpath(GENERATEDDIR)
-cp(joinpath(EXAMPLEDIR, "vtk.jl"), joinpath(GENERATEDDIR, "vtk.jl");
-   force = true)
+
+for f in ("vtk.jl", "advection2d.png")
+  cp(joinpath(EXAMPLEDIR, f), joinpath(GENERATEDDIR, f); force = true)
+end
 
 for input in EXAMPLES
   script = Literate.script(input, GENERATEDDIR)
